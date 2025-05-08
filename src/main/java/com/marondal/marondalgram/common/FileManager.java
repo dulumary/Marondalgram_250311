8942f84686f80e4494851f6966fe92007b592417
dulumary <lecture@hagulu.com>
2025-05-08 19:23:38 +0900

    [김인규]  - aws 배포 세팅

--- src/main/java/com/marondal/marondalgram/common/FileManager.java
+++ src/main/java/com/marondal/marondalgram/common/FileManager.java
@@ -10,7 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class FileManager {
 	
-	public static final String FILE_UPLOAD_PATH = "E:\\dulumaryT\\web\\20241114\\project\\upload\\marondalgram"; 
+	public static final String FILE_UPLOAD_PATH = "/home/ec2-user/upload/marondalgram"; 
 	
 	public static String saveFile(int userId, MultipartFile file) {
 		// 파일 이름 유지 
